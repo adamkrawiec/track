78c401aaeaea9739028dc7f167f21c6c1dbd4a00
adam krawiec <a.krawiec@exlabs.co.uk>
2023-12-24 23:13:42 +0100

    adds user type

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -1,9 +1,11 @@
 import { ref } from 'vue'
+import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
+import type IUser from '../types/user'
 
 export const useUsersStore = defineStore('users', () => {
-  const users = ref([]);
+  const users: Ref<IUser[]> = ref([]);
 
   async function fetchUsers () {
     let data = await axios.get("http://localhost:3000/users");
---
+++ src/types/user.ts
@@ -0,0 +1,7 @@
+export default interface IUser {
+  id: Number,
+  fullName: String,
+  email: String,
+  createdAt: Date,
+  _links: Object
+}
